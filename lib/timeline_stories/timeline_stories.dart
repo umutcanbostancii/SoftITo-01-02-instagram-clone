83d27c446c5ea82b7fc25e246d211a5253258aad
sinif1-07 <sinif1-07@sinif1-07-iMac.local>
2023-03-16 11:08:33 +0300

    timeline stories widget added
    time line stories widget

---
+++ lib/timeline_stories/timeline_stories.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TimeLineStories extends StatefulWidget {
+  const TimeLineStories({super.key});
+
+  @override
+  State<TimeLineStories> createState() => _TimeLineStoriesState();
+}
+
+class _TimeLineStoriesState extends State<TimeLineStories> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [],
+    );
+  }
+}
