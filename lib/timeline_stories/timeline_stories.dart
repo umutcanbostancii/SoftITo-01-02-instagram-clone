ccd4040638b683f9e36c23e5d05e2ef6e89a2a17
Omer Faruk <farukismaloglu0@gmail.com>
2023-03-17 14:31:46 +0300

    timeline stories updated

--- lib/timeline_stories/timeline_stories.dart
+++ lib/timeline_stories/timeline_stories.dart
@@ -21,7 +21,7 @@ class _TimeLineStoriesState extends State<TimeLineStories> {
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: [
-          _storyItem(faker.person.firstName(), faker.image.image()),
+          _storyItemFirst(faker.person.firstName(), faker.image.image()),
           _storyItem(faker.person.firstName(), faker.image.image()),
           _storyItem(faker.person.firstName(), faker.image.image()),
           _storyItem(faker.person.firstName(), faker.image.image()),
@@ -36,43 +36,119 @@ class _TimeLineStoriesState extends State<TimeLineStories> {
 }
 
 Widget _storyItem(String name, String image) {
-  return Column(
-    children: [
-      Container(
-        margin: EdgeInsets.all(6),
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomRight,
-            colors: [
-              Colors.purple,
-              Colors.pink,
-              Colors.orange,
-            ],
-          ),
-        ),
-        child: Container(
-          margin: EdgeInsets.all(3),
-          height: 60,
-          width: 60,
+  return Stack(children: <Widget>[
+    Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(6),
           decoration: BoxDecoration(
             shape: BoxShape.circle,
-            image: DecorationImage(
-              image: NetworkImage(image),
-              fit: BoxFit.cover,
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.purple,
+                Colors.pink,
+                Colors.orange,
+              ],
+            ),
+          ),
+          child: Container(
+            margin: EdgeInsets.all(3),
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: NetworkImage(image),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
         ),
-      ),
-      SizedBox(
-        width: 60,
-        child: Text(
-          name,
-          style: TextStyle(color: Colors.white, fontSize: 12),
-          textAlign: TextAlign.center,
+        SizedBox(
+          child: Text(
+            "views",
+            style: TextStyle(color: Colors.white, fontSize: 12),
+          ),
+        )
+      ],
+    ),
+    Positioned(
+      left: 10,
+      child: Container(
+        child: Center(
+          child: Text(
+            "bored",
+            style: TextStyle(fontSize: 10, color: Colors.white),
+          ),
         ),
-      )
-    ],
-  );
+        width: 60,
+        height: 25,
+        decoration: BoxDecoration(
+            color: Colors.grey, borderRadius: BorderRadius.circular(20)),
+      ),
+    )
+  ]);
+}
+
+Widget _storyItemFirst(String name, String image) {
+  return Stack(children: <Widget>[
+    Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(6),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.purple,
+                Colors.pink,
+                Colors.orange,
+              ],
+            ),
+          ),
+          child: Container(
+            margin: EdgeInsets.all(3),
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: NetworkImage(image),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          child: Text(
+            "Leave a note",
+            style: TextStyle(color: Colors.white, fontSize: 12),
+          ),
+        )
+      ],
+    ),
+    Positioned(
+      left: 40,
+      child: Container(
+        alignment: Alignment.center,
+        child: FittedBox(
+          child: IconButton(
+            icon: Icon(
+              Icons.add,
+              size: 35,
+            ),
+            color: Color.fromARGB(255, 255, 255, 255),
+            onPressed: () {},
+          ),
+        ),
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
+      ),
+    )
+  ]);
 }
